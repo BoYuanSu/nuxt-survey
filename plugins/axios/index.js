6fe8c1e3b339242a6d66197b87dc5848b69e12cc
bysu <paul.su@ohbottech.com>
2021-05-26 17:58:31 +0800

    extend $axios instance, add interceptors

---
+++ plugins/axios/index.js
@@ -0,0 +1,39 @@
+export default ({ redirect, $axios }) => {
+  $axios.onRequest((config) => {
+    console.log('test onRequest', config)
+    // 向请求头中塞入 token
+    config.headers.token = 'xxx'
+    // 向参数中塞入 token
+    let data = {}
+    if (config.method.toUpperCase() === 'GET') {
+      data = config.params || {}
+      data.token = 'sss'
+      config.params = data
+    } else {
+      // POST 需要塞入 data
+      data = config.data || {}
+      data.token = 'sss2'
+      config.data = data
+    }
+    console.log('test onRequest2', config)
+  })
+
+  $axios.onResponse((res) => {
+    // 返回数据逻辑处理
+    console.log('test onResponse', res)
+    if (res.data.code === 1) {
+      // 重定向到 login 页
+      redirect('/login')
+    }
+  })
+
+  $axios.onError((error) => {
+    console.log('Making request to ' + error.response.config.url)
+    const mapStatus = {
+      403: '/error/403',
+      404: '/error/404',
+      500: '/error/500'
+    }
+    redirect(mapStatus[error.response.status])
+  })
+}
